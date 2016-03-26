3fef839d8c52739299b83414250927e1183b9c21
Vladimir37 <vladimir_zapas@mail.ru>
2016-03-26 00:32:59 +0000

    Basis 0.3

--- finam_stock_data.py
+++ finam_stock_data.py
@@ -4,8 +4,13 @@ from datetime import datetime
 from pandas import read_csv, set_option
 
 def get_data(start_date, end_date, period, symbol_code):
-    url_addr = 'http://195.128.78.52/table.csv?'
-    url = "http://195.128.78.52/table.csv?market=5&em=86&code=GBPUSD&df=1&mf=11&yf=2014&from=01.12.2014&dt=6&mt=11&yt=2014&to=06.12.2014&p=2&e=.csv&cn=GBPUSD&dtf=1&tmf=3&MSOR=1&mstime=on&mstimever=1&sep=3&sep2=1&datf=5&at=1"
+    # converting date
+    start_date = datetime.fromtimestamp(start_date)
+    end_date = datetime.fromtimestamp(end_date)
+    start_date_str = start_date.strftime('%d.%m.%Y')
+    end_date_str = end_date.strftime('%d.%m.%Y')
+
+    # converting period
     periods = {
         'tick': 1,
         'min': 2,
@@ -18,26 +23,41 @@ def get_data(start_date, end_date, period, symbol_code):
         'week': 9,
         'month': 10
     }
+    period = periods[period]
+
+    url_addr = 'http://195.128.78.52/table.csv?'
+    url = "http://195.128.78.52/table.csv?market=5&em=86&code=GBPUSD&df=1&mf=11&yf=2014&from=01.12.2014&dt=6&mt=11&yt=2014&to=06.12.2014&p=2&e=.csv&cn=GBPUSD&dtf=1&tmf=3&MSOR=1&mstime=on&mstimever=1&sep=3&sep2=1&datf=5&at=1"
 
     # symbol data from database
     connect = sqlite3.connect('symbols.sqlite')
     cursor = connect.cursor()
     target_code = (symbol_code,)
     cursor.execute('SELECT * FROM bases WHERE EmitentCode=?', target_code)
-    print(cursor.fetchone())
+    search_result = cursor.fetchone()
+    if not search_result:
+        print('Not found')
+        return False
 
     # creating request
-    # params = urllib.parse.urlencode([('market', 5), ])
-    #
-    # data = read_csv(url, header=0, index_col=0, parse_dates={'Date&Time': [0, 1]}, sep=';').sort_index()
-    # data.columns = ['' + i for i in ['Open', 'High', 'Low', 'Close', 'Volume']]
-    #
-    # set_option('display.max_columns', 50)
-    # set_option('display.width', 500)
-    #
-    # # table = data.head(5).to_dict()['Open'][pandas.to_datetime('2014-12-01 00:01:00')]
-    # table = data.head(5)
-    # print(table.to_json())
+    params = urllib.parse.urlencode([('market', 5), ('em', search_result[1]), ('code', search_result[3]),
+                 ('df', start_date.day), ('mf', start_date.month - 1), ('yf', start_date.year),
+                 ('from', start_date_str),
+                 ('dt', end_date.day), ('mt', end_date.month - 1), ('yt', end_date.year),
+                 ('to', end_date_str),
+                 ('p', period), ('f', "table"), ('e', ".csv"), ('cn', search_result[1]),
+                 ('dtf', 1), ('tmf', 3), ('MSOR', 1), ('mstime', "on"), ('mstimever', 1),
+                 ('sep', 3), ('sep2', 1), ('datf', 5), ('at', 1)
+                                     ])
+
+    # requestint
+    data = read_csv(url_addr + params, header=0, index_col=0, parse_dates={'Date&Time': [0, 1]}, sep=';').sort_index()
+    data.columns = ['' + i for i in ['Open', 'High', 'Low', 'Close', 'Volume']]
+
+    set_option('display.max_columns', 50)
+    set_option('display.width', 500)
+
+    # table = data.head(5).to_dict()['Open'][pandas.to_datetime('2014-12-01 00:01:00')]
+    print(data.to_json())
 
 
-get_data(1,1,1,'YHOO')
+qw = get_data(1352678400, 1352937600, 'daily','MSFT')
